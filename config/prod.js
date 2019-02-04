a6137603e26a69bb9f0fafde5d78d90e89a3d2a1
Rudy Sharar <rudy@activelylearn.com>
2019-02-03 20:56:55 -0800

    gitignore update

--- config/prod.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    clientID: process.env.CLIENTID,
-    clientSecret: process.env.CLIENTSECRET,
-    MONGODB_URI: process.env.MONGO_URI,
-    COOKIEKEY: process.env.COOKIE_KEY
-  };
